752e9738ce856989a7e19bee42b3bb1039c995c9
yatto17 <ysht17@gmail.com>
2021-09-15 18:37:54 +0900

    chat(s)コンポーネント、Answer(List)コンポーネントの作成

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import defaultDataset from './dataset';
+import './assets/styles/style.css';
+import { AnswersList, Chats } from './components/index';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: [],
+      chats: [],
+      currentId: "init",
+      dataset: defaultDataset,
+      open: false
+    }
+    this.selectAnswer = this.selectAnswer.bind(this)
+  }
+
+  displayNextQuestion = (nextQuestionId) => {
+    const chats = this.state.chats;
+    chats.push({
+      text: this.state.dataset[nextQuestionId].question,
+      type: 'question'
+    });
+
+    this.setState({
+      answers: this.state.dataset[nextQuestionId].answers,
+      chats: chats,
+      currentId: nextQuestionId
+    })
+  }
+
+  selectAnswer = (selectedAnswer, nextQuestionId) => {
+    switch(true) {
+      case (nextQuestionId === 'init'):
+        this.displayNextQuestion(nextQuestionId)
+        break;
+      default:
+        const chats = this.state.chats;
+        chats.push({
+          text: selectedAnswer,
+          type: 'answer'
+        })
+    
+        this.setState({
+          chats: chats
+        })
+
+        this.displayNextQuestion(nextQuestionId)
+        break;
+    }
+  }
+
+  // initAnswer = () => {
+  //   const initDataset = this.state.dataset[this.state.currentId];
+  //   const initAnswers = initDataset.answers;
+
+  //   this.setState({
+  //     answers: initAnswers
+  //   })
+  // }
+
+  // initChats = () => {
+  //   const initDataset = this.state.dataset[this.state.currentId];
+  //   const chat = {
+  //     text: initDataset.question,
+  //     type: 'question'
+  //   }
+    
+  //   const chats = this.state.chats;
+  //   chats.push(chat);
+
+  //   this.setState({
+  //     chats: chats
+  //   })
+  // }
+
+  componentDidMount() {
+    const initAnswer = "";
+    this.selectAnswer(initAnswer, this.state.currentId)
+  }
+
+  render() {
+    return (
+      <section className='c-section'>
+        <div className='c-box'>
+          <Chats chats={this.state.chats} />
+          <AnswersList answers={this.state.answers} select={this.selectAnswer} />
+        </div>
+      </section>
+    );
+  }
+}
